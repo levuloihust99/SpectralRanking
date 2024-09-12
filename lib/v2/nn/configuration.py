2635540d105e767835c8aaf4053e8e3bd0b29f60
levuloihust99 <levuloihust@gmail.com>
2024-09-13 06:54:34 +0700

    pending training code

---
+++ lib/v2/nn/configuration.py
@@ -0,0 +1,55 @@
+from typing import Literal, Optional
+from pydantic import BaseModel, model_validator
+
+from ..data_helpers.gateway import DataGatewayConfig
+
+
+class CrossEncoderConfig(BaseModel):
+    # tokenizer
+    tokenizer_path: str = "VietAI/vit5-base"
+    sep_token: str = "<extra_id_0>"
+
+    # model
+    model_type: str = "t5"
+    model_path: Optional[str] = None
+
+    # data pipeline
+    data_config: DataGatewayConfig
+
+    # training
+    output_dir: str = "assets/outputs"
+    do_train: bool = True
+    do_eval: bool = True
+    score_scale: float = 1.0
+    learning_rate: float = 2e-5
+    num_train_steps: int = 20000
+    warmup_ratio: float = 0.0
+    warmup_steps: int = 0
+    weight_decay: float = 0.0
+    per_device_train_batch_size: int = 8
+    per_device_eval_batch_size: int = 8
+    adam_eps: float = 1e-8
+    save_steps: int = 100
+    eval_steps: int = 100
+    save_total_limit: int = 2
+    max_grad_norm: float = 1.0
+    metric_for_best_model: str = "eval/acc"
+    greater_is_better: bool = True
+    resume_from_checkpoint: Optional[str] = None
+    seed: Optional[int] = 12345
+
+    # data
+    max_input_len: int = 1500
+
+    # log config
+    log_level: Literal["info", "warning", "debug", "error"] = "info"
+    report_to: list[str] = ["wandb"]
+    wandb_api_key: Optional[str] = None
+    logging_dir: str = "assets/logs"
+    logging_steps: int = 10
+
+    @model_validator(mode="after")
+    def validate_config(self):
+        if self.save_steps % self.eval_steps != 0:
+            raise ValueError("`save_steps` must be multiple of `eval_steps`")
+        return self
---
+++ train_v2.py
@@ -0,0 +1,116 @@
+import json
+import os
+import yaml
+import random
+import string
+import logging
+import argparse
+from pathlib import Path
+from contextlib import contextmanager
+
+import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+from transformers import AutoTokenizer, T5ForConditionalGeneration
+
+from lib.nn.modeling import T5CrossEncoder
+from lib.nn.optimization import get_optimizer
+from lib.utils.logging import do_setup_logging
+from lib.utils.seeding import seed_everything
+from lib.v2.nn.ctx import distributed_context
+from lib.v2.nn.configuration import CrossEncoderConfig
+
+do_setup_logging()
+logger = logging.getLogger(__name__)
+
+
+def find_device():
+    if torch.cuda.is_available():
+        if distributed_context["local_rank"] != -1:
+            device = torch.device("cuda:{}".format(distributed_context["local_rank"]))
+        else:
+            device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    distributed_context["device"] = device
+
+
+@contextmanager
+def may_setup_distributed():
+    local_rank = os.environ.get("LOCAL_RANK")
+    if local_rank is None:
+        distributed_context["local_rank"] = -1
+        distributed_context["world_size"] = 1
+        yield
+    else:
+        local_rank = int(local_rank)
+        world_size = int(os.environ.get("WORLD_SIZE"))
+        dist.init_process_group(backend="nccl", rank=local_rank, world_size=world_size)
+        distributed_context["local_rank"] = local_rank
+        distributed_context["world_size"] = world_size
+        yield
+        dist.destroy_process_group()
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config_file", default=".config.yml")
+    args = parser.parse_args()
+
+    # READ CONFIG
+    with open(args.config_file, "r", encoding="utf-8") as reader:
+        config_dict = yaml.safe_load(reader)
+    config = CrossEncoderConfig(**config_dict)
+
+    # < SETUP LOGGING AND RANDOM
+    run_id = "".join(
+        random.choice(string.digits + string.ascii_uppercase) for _ in range(16)
+    )
+    do_setup_logging(level=config.log_level)
+    if config.seed:
+        seed_everything(config.seed)
+    # SETUP LOGGING AND RANDOM />
+
+    # < WRITE CONFIG
+    output_dir = os.path.join(config.output_dir, run_id)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    with open(
+        os.path.join(output_dir, "training_config.json"), "w", encoding="utf-8"
+    ) as writer:
+        json.dump(config.model_dump(), writer, indent=4, ensure_ascii=False)
+
+    # < INIT TOKENIZER AND MODEL
+    tokenizer = AutoTokenizer.from_pretrained(config.tokenizer_path)
+    pretrained = T5ForConditionalGeneration.from_pretrained(config.model_path)
+    model = T5CrossEncoder.from_t5_for_conditional_generation(pretrained)
+    # INIT TOKENIZER AND MODEL />
+
+    # SETUP MODEL
+    model.to(distributed_context["device"])
+    optimizer = get_optimizer(
+        model,
+        learning_rate=config.learning_rate,
+        adam_eps=config.adam_eps,
+        weight_decay=config.weight_decay,
+    )
+    if distributed_context["local_rank"] != -1:
+        model = DDP(
+            model,
+            device_ids=[distributed_context["local_rank"]],
+            output_device=distributed_context["local_rank"],
+        )
+        
+    # < SETUP DATA PIPELINE
+    # SETUP DATA PIPELINE />
+
+    # < LOAD CHECKPOINT
+    if config.resume_from_checkpoint:
+        pass
+    # LOAD CHECKPOINT />
+
+
+if __name__ == "__main__":
+    with may_setup_distributed():
+        main()
